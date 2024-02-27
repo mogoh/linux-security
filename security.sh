6af39543e385e7847d285338e11d8447f1f039e3
Leonhard Küper <leonhard.kueper@mogoh.de>
2024-02-27 16:42:22 +0100

    security.sh

--- security.sh
+++ security.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo "Please don't \`wget -q -O- https://some.domain/something | sudo bash\` Read the script beforehand or just don't." | toilet -f term -F border --gay
+echo "Please don’t \`wget -q -O- https://some.domain/something | sudo bash\` Read the script beforehand or just don’t." | toilet -f term -F border --gay
